d64ecec9812659cc94826e3ed643f7207f64097b
Emmanuel Valdés Rodríguez <emmvrdz@gmail.com>
2021-03-16 23:45:55 -0600

    first of the primers

---
+++ 1 The Basics.R
@@ -0,0 +1,34 @@
+###### 1 The Basics ######
+
+
+
+#### 1.1 Programming Basics ####
+
+
+# To make something happen in R you call a function................................
+
+# As a new R user we should know how to do three things with the function:
+
+# 1. If you type the name of the function by itself R will return the code that is 
+# stored in the function. This is the code that R runs when you call the function.
+
+sqrt
+
+# 2. To run  the function you must place a pair of () after the name, and if the
+# function needs the input to job place it between the ()
+
+sqrt()
+sqrt(121)
+
+# 3. If you put a ? before the function name and run it R will open a help page of 
+# the function, This is the best way to know how the function works.
+
+?sqrt
+
+
+# Comments
+# R will not run anything after a #. With this you can write human readable comments
+# in your code: just place the comments after the #.
+
+
+# The input that you give the function is call argument............................
